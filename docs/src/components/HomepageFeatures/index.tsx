7ebf87c2a8f34ac6131026d82036cd636bb3c62b
Jiaqi Liu <jack20220723@gmail.com>
2025-05-31 12:34:30 +0800

    Change lincense link from http to https (#36)

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -85,7 +85,7 @@ const FeatureList: FeatureItem[] = [
                   </Link>
               ),
               apache: (
-                  <Link to="http://www.apache.org/licenses/LICENSE-2.0.html">
+                  <Link to="https://www.apache.org/licenses/LICENSE-2.0.html">
                     <Translate id="homepage.feature.opensource.apache">
                       Apache License, Version 2.0
                     </Translate>
